393827822cb73185e81fd881b6ccdd55985e3edd
gui23dib <gui23dib@gmail.com>
2024-11-28 01:37:25 -0300

    add new interface abs foods and settings

--- classes/nutrition_dataframe.py
+++ classes/nutrition_dataframe.py
@@ -2,11 +2,29 @@ import csv
 
 from classes.food import FoodNode
 
+def get_csv(file_path) -> list[FoodNode]:
+    objects = []
+
+    with open(file_path, mode='r', encoding='utf-8') as file:
+        reader = csv.DictReader(file, delimiter=',')
+
+        for row in reader:
+            entry = FoodNode(
+                name=row['Nome'],
+                protein=float(row['Proteína']),
+                calories=float(row['Calorias']),
+                fat=float(row['Gordura']),
+                carbs=float(row['Carboidratos'])
+            )
+            objects.append(entry)
+
+    return objects
+
 class NutritionDataFrame:
-    def __init__(self, foodlist = None, max_calories = 2000, target_protein = 200, target_fat = 50, target_carbs = 100):
+    def __init__(self, foodlist: list[FoodNode] = None, max_calories = 2000, target_protein = 200, target_fat = 50, target_carbs = 100):
         print("Initializing NutritionDataFrame")
         print("max_calories:", max_calories, type(max_calories))
-        self.foodlist: list[FoodNode] = foodlist or self.get_csv('data/foods.csv')
+        self.foodlist: list[FoodNode] = foodlist or get_csv('data/foods.csv')
         
         self.target_protein: int = target_protein
         self.target_fat: int = target_fat
@@ -24,24 +42,6 @@ class NutritionDataFrame:
         if total_calorie_cost > self.max_calories:
             return 0 
         return total_macro
-    
-    def get_csv(self, file_path) -> list[FoodNode]:
-        objects = []
-
-        with open(file_path, mode='r', encoding='utf-8') as file:
-            reader = csv.DictReader(file, delimiter=',')
-
-            for row in reader:
-                entry = FoodNode(
-                    name=row['Nome'],
-                    protein=float(row['Proteína']),
-                    calories=float(row['Calorias']),
-                    fat=float(row['Gordura']),
-                    carbs=float(row['Carboidratos'])
-                )
-                objects.append(entry)
-
-        return objects
 
 if __name__ == '__main__':
     data = NutritionDataFrame()
